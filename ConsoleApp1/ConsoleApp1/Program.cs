7ecd232201ea6570dd244e2447ceadd7bf95dff2
Ярослав Андрієвський <ipz235_ayai@student.ztu.edu.ua>
2025-01-29 18:14:33 +0200

    Sum calculator

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Сума: {sum}");
+        }
+    }
+}
